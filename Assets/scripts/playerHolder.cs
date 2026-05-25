251c926a53644cbcb133a8a7079c8240efbcddfe
Skyhi152 <sydneyneale7@gmail.com>
2026-05-25 23:18:16 +0100

    Sunlight minor change
    changes update to fixedupdate so its not dependent on framerate

--- Assets/scripts/playerHolder.cs
+++ Assets/scripts/playerHolder.cs
@@ -26,7 +26,7 @@ public class playerHolder : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (timerOn == true)
         {
